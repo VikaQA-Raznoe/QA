04e9b44ccc6f4515debe7543666c2349dd8daa29
Vika <tempvika@bk.ru>
2018-05-21 22:42:17 +0300

    add testcucumber222

--- testcucumber222/src/test/java/testcucumber/TestRunner.java
+++ testcucumber222/src/test/java/testcucumber/TestRunner.java
@@ -4,6 +4,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/testcucumber",
--- testcucumber222/target/test-classes/testcucumber/search.feature
+++ testcucumber222/target/test-classes/testcucumber/search.feature
@@ -2,5 +2,4 @@ Feature: google
 Scenario:Finding some cars
     Given I am on the Google search page
     When I search for "Cars"
-    And I click button_enter
     Then The page title contains "Cars"
